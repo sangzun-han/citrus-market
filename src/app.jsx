10157f668e79f9f2da030ddcd06c3b479e5a0f3c
sangzun-han <hsjun1996@naver.com>
2022-01-25 18:35:45 +0900

    ADD : component splash

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,15 @@
 import "./app.css";
+import { Route } from "react-router-dom";
+import Splash from "./components/splash";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Route exact path="/">
+        <Splash />
+      </Route>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/splash.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./splash.module.css";
+
+const Splash = () => {
+  return (
+    <div className={styles.logo - wrap}>
+      <img src="images/full-logo.png" alt="logo" />
+    </div>
+  );
+};
+
+export default Splash;
