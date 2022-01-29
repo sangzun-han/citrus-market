e16f10ae93a9b381057ff0f623a66396ca3e2dce
sangzun-han <hsjun1996@naver.com>
2022-01-29 21:43:41 +0900

    signup finish

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -3,21 +3,30 @@ import { checkEmail } from "../../service/fetcher";
 import styles from "./signup.module.css";
 
 const Signup = () => {
-  const [email, setEmail] = useState(null);
-  const [emailValid, setEmailValid] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [valid, setValid] = useState("");
   const emailRef = useRef();
   const passwordRef = useRef();
 
+  const [email, setEmail] = useState(null);
+  const [emailValid, setEmailValid] = useState(null);
+
+  const [password, setPassword] = useState(null);
+  const [passwordValid, setPasswordValid] = useState(false);
+
+  const [valid, setValid] = useState(false);
   const allInput = valid === true ? styles.on : "";
 
+  // 이메일 유효성 검사
   const emailCheck = () => {
     const reg =
       /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/;
     return reg.test(emailRef.current.value);
   };
 
+  // 비밀번호 유효성 검사
+  const passwordCheck = () =>
+    passwordRef.current.value.length >= 6 ? true : false;
+
+  // 이메일 중복 검사
   const handleEmailValid = (event) => {
     if (emailCheck()) {
       event.preventDefault();
@@ -35,21 +44,24 @@ const Signup = () => {
     }
   };
 
-  const handlePassword = () => {
-    setPassword(passwordRef.current.value);
+  // 패스워드 유효성 검사
+  const handlePasswordValid = () => {
+    if (passwordCheck()) setPasswordValid(true);
+    else setPasswordValid(false);
   };
 
+  // 이메일 중복 유효성 검사 && 패스워드 유효성검사
   useEffect(() => {
-    if (email && password) {
+    if (emailCheck() && passwordCheck() && emailValid) {
       setValid(true);
     } else {
       setValid(false);
     }
-  }, [email, password]);
+  }, [emailValid, emailCheck, passwordCheck]);
+
   return (
     <article>
       <h1 className={styles.title}>이메일로 회원가입</h1>
-
       <section className={styles.input_wrap}>
         <form>
           <div className={styles.account}>
@@ -64,7 +76,7 @@ const Signup = () => {
               onBlur={handleEmailValid}
             />
           </div>
-          <span className={styles.emailValid}>
+          <span className={styles.err}>
             {emailValid
               ? "사용 가능한 이메일 입니다."
               : emailValid === null
@@ -78,10 +90,13 @@ const Signup = () => {
             <input
               ref={passwordRef}
               className={`${styles.input} ${styles.pwd}`}
-              type="text"
+              type="password"
               placeholder="비밀번호를 설정해 주세요"
-              onChange={handlePassword}
+              onChange={handlePasswordValid}
             />
+            <span className={styles.err}>
+              {passwordValid ? "" : "비밀번호는 6자리 이상이어야 합니다."}
+            </span>
           </div>
           <button
             className={`${styles.login_btn}  ${allInput}`}
