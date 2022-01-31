654b19138b0cfb33dfb9dce0c76eb83444cd90f7
sangzun-han <hsjun1996@naver.com>
2022-01-31 22:12:34 +0900

    navbar component

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -4,10 +4,22 @@ const Nav = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.icon_member}>
-        <img src="/images/home/icon-home.png" alt="icon-home" />
-        <img src="/images/home/icon-message-circle.png" alt="icon-chat" />
-        <img src="/images/home/icon-edit.png" alt="icon-edit" />
-        <img src="/images/home/icon-user.png" alt="icon-user" />
+        <button className={styles.btn}>
+          <img src="/images/home/icon-home.png" alt="icon-home" />
+          <span>홈</span>
+        </button>
+        <button className={styles.btn}>
+          <img src="/images/home/icon-message-circle.png" alt="icon-chat" />
+          <span>채팅</span>
+        </button>
+        <button className={styles.btn}>
+          <img src="/images/home/icon-edit.png" alt="icon-edit" />
+          <span>게시물 작성</span>
+        </button>
+        <button className={styles.btn}>
+          <img src="/images/home/icon-user.png" alt="icon-user" />
+          <span>프로필</span>
+        </button>
       </div>
     </footer>
   );
