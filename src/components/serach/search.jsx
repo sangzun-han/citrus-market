9045ca7cafdd44255936a50e0da00b0205279373
sangzun-han <hsjun1996@naver.com>
2022-02-01 19:35:26 +0900

    검색 컴포넌트 생성

---
+++ src/components/serach/search.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import syltes from "./search.module.css";
+const Search = () => {
+  return (
+    <div>
+      <h1>search</h1>
+    </div>
+  );
+};
+
+export default Search;
