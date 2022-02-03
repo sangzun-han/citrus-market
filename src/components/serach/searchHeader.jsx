ea92852bf840419f0be7e96e115605309e9ab96a
sangzun-han <hsjun1996@naver.com>
2022-02-03 20:00:13 +0900

    universal-cookie 설치

--- src/components/serach/searchHeader.jsx
+++ src/components/serach/searchHeader.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import styles from "./searchHeader.module.css";
 const SearchHeader = () => {
+  const history = useHistory();
+  const onSearch = () => {};
   return (
     <header className={styles.search_header}>
-      <div>
+      <div onClick={() => history.goBack()}>
         <img src="/images/search/icon-arrow-left.png" alt="icon-arrow-left" />
       </div>
       <input
         className={styles.input_search}
         type="text"
         placeholder="계정 검색"
+        // onChange={onSearch}
       />
     </header>
   );
