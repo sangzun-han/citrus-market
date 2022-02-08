34c094741d509d38bf5262e8ba83163809fee688
sangzun-han <hsjun1996@naver.com>
2022-02-08 19:26:30 +0900

    상품등록 상품명 유효성검사

--- src/components/productUpload/productUpload.jsx
+++ src/components/productUpload/productUpload.jsx
@@ -1,13 +1,29 @@
-import React from "react";
-
+import React, { useState, useRef } from "react";
+import { getCookie } from "../../service/cookie";
 import ProductHeader from "./productHeader";
 import ProductUploadInfo from "./productUploadInfo";
 
 const ProductUpload = () => {
+  const token = getCookie("token");
+  const accountName = getCookie("accountname");
+
+  const imageRef = useRef();
+  const nameRef = useRef();
+  const priceRef = useRef();
+  const linkRef = useRef();
+
+  const [valid, setValid] = useState(false);
+
   return (
     <>
-      <ProductHeader />
-      <ProductUploadInfo />
+      <ProductHeader valid={valid} />
+      <ProductUploadInfo
+        imageRef={imageRef}
+        nameRef={nameRef}
+        priceRef={priceRef}
+        linkRef={linkRef}
+        setValid={setValid}
+      />
     </>
   );
 };
--- src/components/productUpload/productUploadInfo.jsx
+++ src/components/productUpload/productUploadInfo.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./productUploadInfo.module.css";
 
-const ProductUploadInfo = () => {
+const ProductUploadInfo = ({
+  imageRef,
+  nameRef,
+  priceRef,
+  linkRef,
+  setValid,
+}) => {
+  const [nameValid, setNameValid] = useState(null);
+
+  const checkName = () => {
+    if (nameRef.current.value.length >= 2 && nameRef.current.value.length <= 15)
+      setNameValid(true);
+    else setNameValid(false);
+  };
+
   return (
     <main className={styles.container}>
       <section className={styles.product_img}>
@@ -18,7 +32,7 @@ const ProductUploadInfo = () => {
               src="/images/product/img-button.png"
               alt="img-button"
             />
-            <input type="file" id="product-image" hidden />
+            <input ref={imageRef} type="file" id="product-image" hidden />
           </label>
         </div>
       </section>
@@ -27,15 +41,25 @@ const ProductUploadInfo = () => {
         <div className={styles.product_name}>
           <label htmlFor="productname">상품명</label>
           <input
+            ref={nameRef}
             className={styles.input_info}
             type="text"
             placeholder="2~15자 이내여야 합니다."
+            onBlur={checkName}
           />
+          <span className={styles.err}>
+            {nameValid == null
+              ? ""
+              : !nameValid
+              ? "*사용자 이름은 2~15자 이내여야 합니다."
+              : ""}
+          </span>
         </div>
 
         <div className={styles.product_price}>
           <label htmlFor="price">가격</label>
           <input
+            ref={priceRef}
             className={styles.input_info}
             type="text"
             placeholder="숫자만 입력 가능합니다."
@@ -45,6 +69,7 @@ const ProductUploadInfo = () => {
         <div className={styles.product_link}>
           <label htmlFor="link">판매 링크</label>
           <input
+            ref={linkRef}
             className={styles.input_info}
             type="text"
             placeholder="URL을 입력해 주세요"
