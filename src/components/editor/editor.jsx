f4f1ea0c5fc8f7f6f8b7af06a09a83716b2cf26f
sangzun-han <hsjun1996@naver.com>
2022-02-10 18:13:19 +0900

    textArea 글 길이에 따른 높이조절

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useCallback, useRef, useState } from "react";
 import { Redirect } from "react-router-dom";
 import EditorHeader from "./editorHeader";
-import Nav from "../nav/nav";
 import EditorInfo from "./editorInfo";
 import { getCookie } from "../../service/cookie";
 import { getInfo } from "../../service/fetcher";
@@ -9,7 +8,46 @@ import { getInfo } from "../../service/fetcher";
 const Editor = ({ isLogin }) => {
   const accountName = getCookie("accountname");
   const token = getCookie("token");
+  // 프로필 이미지
   const [image, setImage] = useState("");
+  const [uploadImage, setUploadImage] = useState([]);
+
+  const textAreaRef = useRef();
+  const imageRef = useRef();
+
+  // textarea 높이 조절
+  const handleResizeHeight = useCallback(() => {
+    if (textAreaRef.current.value === null) return;
+    textAreaRef.current.style.height = "38px";
+    textAreaRef.current.style.height = textAreaRef.current.scrollHeight + "px";
+
+    if (textAreaRef.current.scrollHeight > 400) {
+      textAreaRef.current.style.height = 400 + "px";
+      textAreaRef.current.style.overflowY = "auto";
+    }
+  }, []);
+
+  // 이미지 미리보기
+  const imagePreview = (event) => {
+    if (imageRef.current.files.length > 3) {
+      alert("이미지는 최대 3장까지 업로드 가능합니다.");
+      imageRef.current.value = "";
+    } else if (imageRef.current.files.length > 0) {
+      const images = Array.from(imageRef.current.files);
+      images.forEach((_, index) => {
+        let reader = new FileReader();
+        reader.onloadend = (e) => {
+          setUploadImage((uploadImage) => [...uploadImage, e.target.result]);
+        };
+        reader.readAsDataURL(event.target.files[index]);
+      });
+    }
+  };
+
+  const deleteImagePreview = () => {
+    setUploadImage([]);
+    imageRef.current.value = "";
+  };
 
   useEffect(() => {
     getInfo(accountName, token).then((res) => {
@@ -20,8 +58,15 @@ const Editor = ({ isLogin }) => {
   return (
     <>
       <EditorHeader />
-      <EditorInfo image={image} />
-      <Nav />
+      <EditorInfo
+        image={image}
+        textAreaRef={textAreaRef}
+        imageRef={imageRef}
+        handleResizeHeight={handleResizeHeight}
+        uploadImage={uploadImage}
+        imagePreview={imagePreview}
+        deleteImagePreview={deleteImagePreview}
+      />
     </>
   );
 };
--- src/components/editor/editorInfo.jsx
+++ src/components/editor/editorInfo.jsx
@@ -1,28 +1,63 @@
-import React, { useRef } from "react";
+import React from "react";
 import styles from "./editorInfo.module.css";
-const EditorInfo = ({ image }) => {
-  const textAreaRef = useRef();
-
-  const handleResizeHeight = () => {
-    console.log(textAreaRef.current.value);
-    if (textAreaRef.current.value === null) return;
-    textAreaRef.current.style.height = "38px";
-    textAreaRef.current.style.height = textAreaRef.current.scrollHeight + "px";
-  };
+const EditorInfo = ({
+  image,
+  textAreaRef,
+  imageRef,
+  handleResizeHeight,
+  uploadImage,
+  imagePreview,
+  deleteImagePreview,
+}) => {
   return (
     <main className={styles.container}>
       <section className={styles.content}>
-        <div className={styles.profile_img}>
-          <img src={image} alt="profile" />
+        <div className={styles.info_wrap}>
+          <div className={styles.profile_img}>
+            <img src={image} alt="profile" />
+          </div>
+          <div className={styles.variable_height}>
+            <textarea
+              ref={textAreaRef}
+              className={styles.input_area}
+              placeholder="게시글 입력하기..."
+              onChange={handleResizeHeight}
+            />
+          </div>
         </div>
-        <div className={styles.variable_height}>
-          <textarea
-            ref={textAreaRef}
-            className={styles.input_area}
-            placeholder="게시글 입력하기..."
-            onChange={handleResizeHeight}
-          />
+        <div className={styles.upload_wrap}>
+          {uploadImage.length === 1 ? (
+            <div className={styles.image_slide}>
+              <img
+                className={styles.upload_img}
+                src={uploadImage}
+                alt="upload"
+              />
+              <img
+                className={styles.btn_delete}
+                src="/images/editor/icon-delete.png"
+                alt="delete"
+                onClick={deleteImagePreview}
+              />
+            </div>
+          ) : (
+            <div className={styles.image_slides}>
+              <img src={uploadImage} alt="uploads" />
+            </div>
+          )}
         </div>
+        <label className={styles.btn_upload_img} htmlFor="upload_img">
+          <img src="/images/basic/upload-file.png" alt="upload_img" />
+        </label>
+        <input
+          ref={imageRef}
+          type="file"
+          id="upload_img"
+          accept="image/*"
+          hidden
+          multiple
+          onChange={imagePreview}
+        />
       </section>
     </main>
   );
