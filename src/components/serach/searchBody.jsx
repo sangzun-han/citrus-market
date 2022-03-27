87dd06a30797dfe38ab0be21bb57ff7d225f7c58
sangzun-han <hsjun1996@naver.com>
2022-03-28 01:30:24 +0900

    fix : get image

--- src/components/serach/searchBody.jsx
+++ src/components/serach/searchBody.jsx
@@ -5,6 +5,15 @@ import styles from "./searchBody.module.css";
 
 const SearchBody = ({ userData, searchRef }) => {
   const { image, username, accountname } = userData;
+
+  const getImage = () => {
+    if (image.indexOf(API_END_POINT) !== -1 && image.length >= 50)
+      return `/images/profile/Ellipse-1.png`;
+    else if (image.indexOf(API_END_POINT) !== -1) return image;
+    else if (image.length >= 50) return `/images/profile/Ellipse-1.png`;
+    else return `/images/profile/Ellipse-1.png`;
+  };
+
   const highlight = (username) => {
     const start = username.indexOf(searchRef.current.value);
     const end = searchRef.current.value.length + start;
@@ -26,11 +35,7 @@ const SearchBody = ({ userData, searchRef }) => {
     <Link to={`/user-profile/${accountname}`}>
       <section className={styles.wrap}>
         <div className={styles.profile_img}>
-          {image.includes(`${API_END_POINT}`) ? (
-            <img src={image} alt="profile" />
-          ) : (
-            <img src={`${API_END_POINT}/${image}`} alt="post-img" />
-          )}
+          <img src={getImage()} alt="profile" />
         </div>
         <div className={styles.info}>
           {highlight(username)}
